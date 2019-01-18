21ff7c237a79872ba1d96e0442f4ab77e14f6f31
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-01-18 17:22:24 -0500

    Added basic react example

---
+++ Controllers/ReactController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactLab.Models;
+
+namespace ReactLab.Controllers
+{
+    public class ReactController : Controller{
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
