bcdfcc3519be39e93f810e76454212523ef8377e
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-02-21 17:13:37 -0500

    Finished D3 tutorial

--- src/js/components/container/HistorySkiingContainer.jsx
+++ src/js/components/container/HistorySkiingContainer.jsx
@@ -41,6 +41,23 @@ const historicDatesForSkiing = [
     }
 ]
 
+const Canvas = ({ children }) =>
+    <svg height="200" width="500">
+        {children}
+    </svg>
+
+const TimelineDot = ({ position, txt }) =>
+    <g transform={`translate(${position}, 0)`}>
+        <circle cy={160}
+                r={5}
+                style={{fill: 'blue'}} />
+        
+        <text y={115}
+              x={-95}
+              transform="rotate(-45)"
+              style={{fontSize: '10px'}}>{txt}</text>
+    </g>
+
 class Timeline extends Component {
     constructor({data = []}) {
         super({data});
@@ -48,48 +65,24 @@ class Timeline extends Component {
         const times = d3.extent(data.map(d => d.year));
         const range = [50, 450];
 
+        this.scale = d3.time.scale().domain(times).range(range);
+
         this.state = { data, times, range };
     }
 
-    componentDidMount() {
-        let group;
-        const { data, times, range } = this.state;
-        const { target } = this.refs;
-        const scale = d3.time.scale().domain(times).range(range);
-
-        d3.select(target)
-            .append('svg')
-            .attr('height', 200)
-            .attr('width', 500);
-
-        group = d3.select(target.children[0])
-            .selectAll('g')
-            .data(data)
-            .enter()
-            .append('g')
-            .attr(
-                'transform',
-                (d, i) => 'translate(' + scale(d.year) + ', 0)'
-            );
-        
-        group.append('circle')
-            .attr('cy', 160)
-            .attr('r', 5)
-            .style('fill', 'blue');
-        
-        group.append('text')
-            .text(d => d.year + ' - ' + d.event)
-            .style('font-size', 10)
-            .attr('y', 115)
-            .attr('x', -95)
-            .attr('transform', 'rotate(-45)');
-    }
-
     render() {
+        const { data } = this.state;
+        const { scale } = this;
+
         return (
             <div className="timeline">
                 <h1>{this.props.name} Timeline</h1>
-                <div ref="target"></div>
+                <Canvas>
+                    {data.map((d, i) =>
+                        <TimelineDot position={scale(d.year)}
+                            txt={`${d.year} - ${d.event}`} />
+                    )}
+                </Canvas>
             </div>
         );
     }
@@ -98,7 +91,7 @@ class Timeline extends Component {
 render(
     <Timeline name="History of Skiing"
         data={historicDatesForSkiing} />,
-    document.getElementById('create-article-form"')
+    document.getElementById('create-article-form')
 );
 
 export default Timeline;
