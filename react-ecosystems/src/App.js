31cd52573411d0552388cb10abfd518e9293c7ad
Nikolai Villarin <nvillarin@goeasy.com>
2021-04-14 01:41:58 +0000

    Added UI Components

--- react-ecosystems/src/App.js
+++ react-ecosystems/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
+import TodoList from './todos/TodoList';
+
+const todos = {};
 
 const App = () => (
   <div className="App">
-    <h1>Hello, World!</h1>
+    <TodoList />
   </div>
 );
 
-export default App;
+export default hot(module)(App);
