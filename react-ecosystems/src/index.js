653c643312cff4286001acb011d42f3b8f8290d8
Nikolai Villarin <nvillarin@goeasy.com>
2021-04-16 01:47:47 +0000

    Added Redux_Persist

--- react-ecosystems/src/index.js
+++ react-ecosystems/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App.js';
 import { configureStore } from './store';
 import { Provider } from 'react-redux';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDom.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate
+      loading={<div>Loading...</div>}
+      persistor={persistor}
+    >
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
